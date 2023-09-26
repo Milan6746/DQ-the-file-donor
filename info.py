eee384bc3a59feeea74f3a3b61590857851deee4
Milan6746 <132981562+Milan6746@users.noreply.github.com>
2023-09-26 19:01:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = ('Media_search')
-API_ID = ['29854276']
-API_HASH = ['fae0a38ee4250ca42454f6106460772b']
+API_ID = ['24145498']
+API_HASH = ['8317840cfdbe5b0b64df1ce132435097']
 BOT_TOKEN =['6010153920:AAE2NoPnEzo4USp667-Z2gFw9quJeUYewcw']
 
 # Bot settings
